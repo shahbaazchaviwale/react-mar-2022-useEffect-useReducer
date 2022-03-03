4d0d06e9a68382371630707fe634fea01600a7a5
Shahbaaz <shahbaazc@gmail.com>
2022-03-03 21:12:05 +0530

    added useEffects()

--- src/App.js
+++ src/App.js
@@ -6,23 +6,31 @@ import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
 
-
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   
+  useEffect(() =>{
+    const userLggedIn = localStorage.getItem('isLoggedIn');
 
- 
-
+  if(userLggedIn === '1' ){
+    setIsLoggedIn(true);
+  }
+  console.log('useEffect>>')
+  }, []);
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
     setIsLoggedIn(true);
+    localStorage.setItem('isLoggedIn', '1');
   };
 
   const logoutHandler = () => {
     setIsLoggedIn(false);
-  };
+    localStorage.removeItem('isLoggedIn', '0');
 
+
+  };
+  console.log('useEffect-2>>');
   return (
     <React.Fragment>
       <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
